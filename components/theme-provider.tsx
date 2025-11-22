5d4d1da8d49e260a29c1edfa463f790b99cf7f70
aasasdsadffsf <138111077+charishhh@users.noreply.github.com>
2025-11-22 13:47:59 +0530

    Update theme-provider configuration

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,19 +1,10 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-type ThemeProviderProps = {
-  children: React.ReactNode;
-  attribute?: string;
-  defaultTheme?: string;
-  enableSystem?: boolean;
-  storageKey?: string;
-  themes?: string[];
-  forcedTheme?: string;
-  enableColorScheme?: boolean;
-  disableTransitionOnChange?: boolean;
-}
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps
+} from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
